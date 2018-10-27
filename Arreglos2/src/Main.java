6fe3a22999284b2f6312cc364209c552cd8ef49a
DanielGuzmanT <jhairguz.t@gmail.com>
2018-10-27 11:00:38 -0500

    arreglos 2 hecho

---
+++ Arreglos2/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        * Aumentemos el level de los arreglos
+        * Trabaja con arreglos para almacenar mas datos de los 5 pokemons
+        * */
+        // TODO crea arreglos para 3 pokemons:
+        // buffer es el lector de palabras que ingresas por la terminal
+        Scanner buffer = new Scanner(System.in);
+//        String name = buffer.nextLine();
+//        System.out.println(name);
+
+        // TODO(1) un arreglo de nombres para los 3 pokemons (arreglo de STRINGs)
+
+        // TODO(2) un arreglo de tipos para los 3 pokemons (arreglo de STRINGs)
+
+        // TODO(3) una matriz 3x2 para la salud y para el daño de ataque (arreglo de INTs)
+
+        // TODO(4) recoge datos de 3 pokemons y almacenalos en los arreglos correspondientes
+        for (int i=0; i<3; i++){
+            /*
+             * tu código aquí
+             * */
+        }
+
+        // TODO(5) muestra a cada pokemon con sus datos
+        for (int i=0; i<3; i++){
+            /*
+             * tu código aquí
+             * */
+        }
+    }
+}
