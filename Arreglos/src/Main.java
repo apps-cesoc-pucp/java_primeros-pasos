a957adf3d7ab768f3fc5f8e907da33e58115119b
DanielGuzmanT <jhairguz.t@gmail.com>
2018-10-27 10:42:55 -0500

    Arreglos hecho

---
+++ Arreglos/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Crea una lista de tu pokemons e imprimelos
+        // buffer es el lector de palabras que ingresas por la terminal
+        Scanner buffer = new Scanner(System.in);
+//        String name = buffer.nextLine();
+//        System.out.println(name);
+
+        // TODO(1) crea un arreglo de Strings para almacenar 5 pokemons
+
+        // TODO(2) coloca el codigo necesario dentro del bucle para ALMACENAR 5 nombres de pokemons
+        for (int i=0; i<5; i++){
+            /*
+            * tu código aquí
+            * */
+        }
+
+        // TODO(3) coloca el codigo necesario dentro del bucle para IMPRIMIR los 5 nombres de los pokemons
+        for (int i=0; i<5; i++){
+            /*
+             * tu código aquí
+             * */
+        }
+    }
+}
