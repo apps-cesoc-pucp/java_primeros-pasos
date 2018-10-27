8094faea14583b6dabb83fb9d81c26de9ae417a6
DanielGuzmanT <jhairguz.t@gmail.com>
2018-10-26 21:44:09 -0500

    hito 1: holamundo hecho

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    // TODO(1) Imprimir un HolaMundo en pantalla
+
+    // TODO(2) Imprimir un HolaMundo gigante
+    // link para generar un banner: http://patorjk.com/software/taag/#p=testall&f=Graffiti&t=Hols
+}
