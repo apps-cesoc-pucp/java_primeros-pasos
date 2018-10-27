ed8ade3ac91499ab276f0d76823c8924b521b9be
DanielGuzmanT <jhairguz.t@gmail.com>
2018-10-26 22:20:36 -0500

    Operadores hecho

---
+++ OperadoresAritmeticos/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        * En este ejercicio, se deberá implementar cómo manejar
+        * la salud de un pokemon cuando es atacado
+        * */
+
+        // datos del primer pokemon
+        String pikachu_nombre = "pikachu";
+        int pikachu_ataque  = 15;
+        int pikachu_vida    = 100;
+
+        // datos del segundo pokemon
+        String charmander_nombre = "pikachu";
+        int charmander_ataque  = 30;
+        int charmander_vida    = 80;
+
+        // TODO: SIMULAR BATALLA POKEMON
+        // TODO(1) pikachu ataca 3 veces a charmander (usar '*' y '-=')
+
+        // TODO(2) charmander se enoja y realiza ataque definitivo, deja a pikachu con poca vida (usar '%=')
+
+        // TODO(3) reportar la salud de pikachu y charmander con voz de réferi
+
+        // TODO(4) pikachu se ha iluminado y ahora sabe realizar ataques psíquicos. Ahora realiza un cambio de vida
+        // (pikachu tiene la cantidad de vida de charmander, y charmander, la cantidad de vida de pikachu)
+
+        // TODO(5) imprimir sorpresa de réferi por habilidad psíquica de pikachu
+
+        // TODO(6) pikachu ataca a charmander
+
+        // TODO(7) declarar a ganador con voz de réferi
+
+    }
+}
