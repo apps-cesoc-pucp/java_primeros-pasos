d79df6b11a663f472d13ad0ed12694d8b21fee79
jeancarloslima1 <jean.lima@pucp.edu.pe>
2018-10-28 00:10:29 -0500

    Primer Problema de Arreglos

--- Arreglos/src/Main.java
+++ Arreglos/src/Main.java
@@ -3,26 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Crea una lista de tu pokemons e imprimelos
+        // TODO Crea una lista de tus pokemons e imprimelos
         // buffer es el lector de palabras que ingresas por la terminal
         Scanner buffer = new Scanner(System.in);
-//        String name = buffer.nextLine();
-//        System.out.println(name);
+        System.out.println("Inscriba a sus pókemones: ");
 
         // TODO(1) crea un arreglo de Strings para almacenar 5 pokemons
-
+        String equipo[]=new String[5];
         // TODO(2) coloca el codigo necesario dentro del bucle para ALMACENAR 5 nombres de pokemons
         for (int i=0; i<5; i++){
-            /*
-            * tu código aquí
-            * */
+            String name = buffer.nextLine();
+            equipo[i]=name;
         }
-
         // TODO(3) coloca el codigo necesario dentro del bucle para IMPRIMIR los 5 nombres de los pokemons
+        System.out.println("Tus pókemones son: ");
         for (int i=0; i<5; i++){
-            /*
-             * tu código aquí
-             * */
+            System.out.println((i+1)+") "+equipo[i]);
         }
     }
 }
